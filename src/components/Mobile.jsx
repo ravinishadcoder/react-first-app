3ac174d0b12a56f52c64434394613539301ef3e9
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-05-16 23:21:17 +0530

    first-app

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hello = ()=>{
+    return <div>
+        <h2>Mobile Operating System</h2>
+       <div> <li>Android</li>
+        <li>Blackberry</li>
+        <li>I-phone</li>
+        <li>Windows Phone</li>
+        </div>
+        <h2>Mobile Manufacturers</h2>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Apple</li>
+        <li>Micromax</li>
+    </div>
+    
+}
+
+export default Hello
